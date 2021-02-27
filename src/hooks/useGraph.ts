a77a10560fa51b6f909314587fe99e05f79ffd6d
Brandon Tsang <brandononline2@gmail.com>
2021-02-26 19:41:19 -0500

    Redid panning algorithm.

--- src/hooks/useGraph.ts
+++ src/hooks/useGraph.ts
@@ -2,13 +2,11 @@ import {MutableRefObject, useEffect} from "react";
 
 import {useInput} from "./useInput";
 
-let xmin = -10;
-let xmax = 10;
-let ymin = -10;
-let ymax = 10;
+let center = [0, 0]; // Position of center of screen
+let zoom = 1;
 let width = 1024; // Temporary; will be set later
 let height = 1024; // Temporary; will be set later
-let pxPerUnit = 1;
+const pxPerUnit = 100;
 let d = 1; // devicePixelRatio
 
 export function useGraph(canvas: MutableRefObject<HTMLCanvasElement>) {
@@ -16,20 +14,31 @@ export function useGraph(canvas: MutableRefObject<HTMLCanvasElement>) {
   useEffect(() => {
     if (!canvas.current) return;
 
+    // Convert a grid x-coordinate to a pixel x-coordinate
     function x(val: number) {
-      return Math.floor(((val - xmin) / (xmax - xmin)) * width);
+      return Math.floor(width / 2 - (val - center[0]) * pxPerUnit);
     }
 
+    // Convert a grid y-coordinate to a pixel y-coordinate
     function y(val: number) {
-      return Math.floor(height - ((val - ymin) / (ymax - ymin)) * height);
+      return Math.floor(height / 2 - (val - center[1]) * 100);
+    }
+
+    // Inverse of x()
+    function xi(px: number) {
+      return (px - width / 2) / 100 + center[0];
+    }
+
+    // Inverse of y()
+    function yi(px: number) {
+      return (height / 2 - px) / pxPerUnit + center[1];
     }
 
     function drawGrid(c: CanvasRenderingContext2D) {
       // Vertical lines
-      for (let i = Math.floor(xmin); i < Math.ceil(xmax); i++) {
+      for (let i = Math.floor(xi(0)); i < Math.ceil(xi(width)); i++) {
         c.lineWidth = (i % 5 === 0 ? 2 : 1) * d;
         c.strokeStyle = i % 5 === 0 ? "#444" : "#888";
-
         c.beginPath();
         c.moveTo(x(i), 0);
         c.lineTo(x(i), height);
@@ -37,10 +46,9 @@ export function useGraph(canvas: MutableRefObject<HTMLCanvasElement>) {
       }
 
       // Horizontal lines
-      for (let i = Math.floor(ymin); i < Math.ceil(ymax); i++) {
+      for (let i = Math.floor(yi(height)); i < Math.ceil(yi(0)); i++) {
         c.lineWidth = (i % 5 === 0 ? 2 : 1) * d;
         c.strokeStyle = i % 5 === 0 ? "#444" : "#888";
-
         c.beginPath();
         c.moveTo(0, y(i));
         c.lineTo(width, y(i));
@@ -54,26 +62,22 @@ export function useGraph(canvas: MutableRefObject<HTMLCanvasElement>) {
     function calculateDims() {
       width = canvas.current.getBoundingClientRect().width * d;
       height = canvas.current.getBoundingClientRect().height * d;
-      pxPerUnit = height / (ymax - ymin) / d;
       canvas.current.width = width;
       canvas.current.height = height;
-
-      // Recalculate x bounds to get proper graph aspect ratio
-      const ratio = width / height;
-      const newXRange = (ymax - ymin) * ratio;
-      xmin = -newXRange / 2;
-      xmax = newXRange / 2;
     }
     window.addEventListener("resize", calculateDims);
 
     const c = canvas.current.getContext("2d");
 
     function handleTick() {
-      const [velX, velY, zoom] = getVelocity();
-      xmin -= velX / pxPerUnit;
-      xmax -= velX / pxPerUnit;
-      ymin += velY / pxPerUnit;
-      ymax += velY / pxPerUnit;
+      const [velX, velY, newZoom] = getVelocity();
+
+      center = [
+        center[0] + (velX / pxPerUnit) * d,
+        center[1] + (velY / pxPerUnit) * d,
+      ];
+      // zoom = newZoom;
+
       c.clearRect(0, 0, width, height);
       drawGrid(c);
     }
